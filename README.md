479cf0343cb9869666a6c517aff7bc67d2903d63
deceitya <49361292+deceitya@users.noreply.github.com>
2024-08-11 21:58:37 +0900

    名前、所持金、レベルを履歴管理にした

--- README.md
+++ README.md
@@ -9,26 +9,54 @@
 | --- | --- | --- | :---: | --- | :---: | :---: | :---: | :---: | --- | --- |
 | ID| id | INTEGER UNSIGNED | ○ |  |  |  | ○ |  |  |  |
 | プレイヤーUUID | uuid | BINARY(16) |  |  | ○ | ○ |  | ○ |  |  |
-| プレイヤー名 | name | VARCHAR(16) |  |  | ○ | ○ |  | ○ |  |  |
-| 所持金額 | money | INTEGER UNSIGNED |  |  | ○ |  |  |  | 0 |  |
-| レベル | level | FLOAT UNSIGNED |  |  | ○ |  |  |  | 0 |  |
 | 作成日 | created_at | DATETIME |  |  | ○ |  |  |  | CURRENT_TIMESTAMP |  |
-| 更新日 | updated_at | DATETIME |  |  | ○ |  |  |  | CURRENT_TIMESTAMP | ON UPDATE CURRENT_TIMESTAMP |
 
-### game_histories(ゲーム履歴)
+### name_histories(プレイヤー名更新履歴)
+| 論理カラム名 | 物理カラム名 | 型 | PK | FK | NN | UNIQUE | A_I | INDEX | DEFAULT | 備考 |
+| --- | --- | --- | :---: | :---: | :---: | :---: | :---: | :---: | --- | --- |
+| ID | id | INTEGER UNSIGNED | ○ |  |  |  | ○ |  |  |  |
+| プレイヤーID | player | INTEGER UNSIGNED |  | players.id | ○ |  |  |  |  |  |
+| 更新前名前 | old_name | VARCHAR(16) |  |  | ○ |  |  |  |  |  |
+| 更新後名前 | new_name | VARCHAR(16) |  |  | ○ |  |  |  |  |  |
+| 更新理由 | reason | VARCHAR(50) |  |  |  |  |  |  |  |  |
+| 作成日 | created_at | DATETIME |  |  | ○ |  |  |  | CURRENT_TIMESTAMP |  |
+
+### money_histories(所持金更新履歴)
+| 論理カラム名 | 物理カラム名 | 型 | PK | FK | NN | UNIQUE | A_I | INDEX | DEFAULT | 備考 |
+| --- | --- | --- | :---: | :---: | :---: | :---: | :---: | :---: | --- | --- |
+| ID | id | INTEGER UNSIGNED | ○ |  |  |  | ○ |  |  |  |
+| プレイヤーID | player | INTEGER UNSIGNED |  | players.id | ○ |  |  |  |  |  |
+| 更新前金額 | old_money | INTEGER UNSIGNED |  |  | ○ |  |  |  |  |  |
+| 更新後金額 | new_money | INTEGER UNSIGNED |  |  | ○ |  |  |  |  |  |
+| 更新理由 | reason | VARCHAR(50) |  |  |  |  |  |  |  |  |
+| 作成日 | created_at | DATETIME |  |  | ○ |  |  |  | CURRENT_TIMESTAMP |  |
+
+### level_histories(レベル更新履歴)
+| 論理カラム名 | 物理カラム名 | 型 | PK | FK | NN | UNIQUE | A_I | INDEX | DEFAULT | 備考 |
+| --- | --- | --- | :---: | :---: | :---: | :---: | :---: | :---: | --- | --- |
+| ID | id | INTEGER UNSIGNED | ○ |  |  |  | ○ |  |  |  |
+| プレイヤーID | player | INTEGER UNSIGNED |  | players.id | ○ |  |  |  |  |  |
+| 更新前レベル | old_level | FLOAT UNSIGNED |  |  | ○ |  |  |  |  |  |
+| 更新後レベル | new_level | FLOAT UNSIGNED |  |  | ○ |  |  |  |  |  |
+| 更新理由 | reason | VARCHAR(50) |  |  |  |  |  |  |  |  |
+| 作成日 | created_at | DATETIME |  |  | ○ |  |  |  | CURRENT_TIMESTAMP |  |
+
+### game_histories(ゲーム開催履歴)
 | 論理カラム名 | 物理カラム名 | 型 | PK | FK | NN | UNIQUE | A_I | INDEX | DEFAULT | 備考 |
 | --- | --- | --- | :---: | :---: | :---: | :---: | :---: | :---: | --- | --- |
 | ID | id | INTEGER UNSIGNED | ○ |  |  |  | ○ |  |  |  |
 | 開始日時 | began_at | DATETIME |  |  |  |  |  |  |  |  |
 | 終了日時 | ended_at | DATETIME |  |  |  |  |  |  |  |  |
+| 作成日 | created_at | DATETIME |  |  | ○ |  |  |  | CURRENT_TIMESTAMP |  |
 
-### join_destroying_core_histories(コア破壊参加履歴)
+### join_game_histories(ゲーム参加履歴)
 | 論理カラム名 | 物理カラム名 | 型 | PK | FK | NN | UNIQUE | A_I | INDEX | DEFAULT | 備考 |
 | --- | --- | --- | :---: | --- | :---: | :---: | :---: | :---: | --- | --- |
 | ID | id | INTEGER UNSIGNED | ○ |  |  |  | ○ |  |  |  |
 | ゲームID | game | INTEGER UNSIGNED |  | game_histories.id | ○ |  |  |  |  |  |
 | プレイヤーID | player | INTEGER UNSIGNED |  | players.id | ○ |  |  |  |  |  |
-| チーム | team | INTEGER UNSIGNED | 
+| チーム | team | INTEGER UNSIGNED |  |  | ○ |  |  |  |  |  |
+| 作成日 | created_at | DATETIME |  |  | ○ |  |  |  | CURRENT_TIMESTAMP |  |
 
 ### destroy_core_histories(コア破壊履歴)
 | 論理カラム名 | 物理カラム名 | 型 | PK | FK | NN | UNIQUE | A_I | INDEX | DEFAULT | 備考 |
@@ -46,5 +74,3 @@
 | 死亡プレイヤーID | dead | INTEGER UNSIGNED |  | players.id | ○ |  |  |  |  | 死んだプレイヤーのplayers.id |
 | キルプレイヤー | killer | INTEGER UNSIGNED |  | players.id |  |  |  |  |  | 死んだ原因が他のプレイヤーから殺された場合のみ、殺してきたプレイヤーのplayers.id |
 | 作成日 | created_at | DATETIME |  |  | ○ |  |  |  | CURRENT_TIMESTAMP |  |
-
-### 
--- src/deceitya/corepvp/database/impl/PlayerGameInfoProviderImpl.php
+++ src/deceitya/corepvp/database/impl/PlayerGameInfoProviderImpl.php
@@ -8,36 +8,61 @@ use deceitya\corepvp\database\Database;
 use deceitya\corepvp\database\PlayerGameInfoProvider;
 use deceitya\corepvp\player\PlayerGameInfo;
 use PDO;
+use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
 class PlayerGameInfoProviderImpl implements PlayerGameInfoProvider
 {
-    /**
-     * プレイヤーが参加していたゲームを取得
-     * そのゲームのコア破壊回数をチームごとに集計して
-     * プレイヤーが参加しているチームのほうがコア破壊数が多い場合の数を勝利とする
-     * この勝利数を取得
-     */
     private const SELECT_QUERY = <<<__SQL__
     SELECT
         player.id                       AS id,
         player.uuid                     AS uuid,
-        player.name                     AS name,
-        player.money                    AS money,
-        player.level                    AS level,
+        latest_name.name                AS name,
+        latest_money.money              AS money,
+        latest_level.level              AS level,
         COUNT(DISTINCT kill.id)         AS kill_count,
         COUNT(DISTINCT death.id)        AS death_count,
         COUNT(DISTINCT join.id)         AS play_count,
-        COUNT(DISTINCT destroy_core.id) AS destroy_core_count,
-        (
-            SELECT
-            FROM join_destroying_core_histories 
-        )
-    FROM players                               AS player
-    INNER JOIN join_destroying_core_histories  AS join         ON join.player         = player.id
-    INNER JOIN destroy_core_histories          AS destroy_core ON destroy_core.player = player.id
-    INNER JOIN death_histories                 AS death        ON death.dead          = player.id
-    INNER JOIN death_histories                 AS kill         ON kill.killer         = player.id
+        COUNT(DISTINCT destroy_core.id) AS destroy_core_count
+    FROM players                      AS player
+
+    -- プレイヤーの最新の名前を取得
+    INNER JOIN (
+        SELECT
+            name.player   AS player
+            name.new_name AS name
+        FROM name_histories AS name
+        WHERE name.player = player.id
+        ORDER BY created_at DESC
+        LIMIT 1
+    )                                 AS latest_name  ON latest_name.player  = player.id
+
+    -- プレイヤーの最新の所持金を取得
+    INNER JOIN (
+        SELECT
+            money.player    AS player
+            money.new_money AS money
+        FROM money_histories AS money
+        WHERE money.player = player.id
+        ORDER BY created_at DESC
+        LIMIT 1
+    )                                 AS latest_moeny ON latest_moeny.player = player.id
+
+    -- プレイヤーの最新のレベルを取得
+    INNER JOIN (
+        SELECT
+            level.player    AS player
+            level.new_level AS level
+        FROM level_histories AS level
+        WHERE level.player = player.id
+        ORDER BY created_at DESC
+        LIMIT 1
+    )                                 AS latest_level ON latest_level.player = player.id
+
+    INNER JOIN join_game_histories    AS join         ON join.player         = player.id
+    INNER JOIN destroy_core_histories AS destroy_core ON destroy_core.player = player.id
+    INNER JOIN death_histories        AS death        ON death.dead          = player.id
+    INNER JOIN death_histories        AS kill         ON kill.killer         = player.id
 
     __SQL__;
 
@@ -51,43 +76,125 @@ class PlayerGameInfoProviderImpl implements PlayerGameInfoProvider
         $this->db = $db;
     }
 
+    /**
+     * プレイヤーIDからデータを取得
+     *
+     * @param integer $id
+     * @return PlayerGameInfo|null
+     */
     public function find(int $id): ?PlayerGameInfo
     {
         // IDでフィルターをかける
-        $filtered = array_filter($this->infos, function($infoID) use ($id) {
-            return $infoID === $id;
+        $filtered = array_filter($this->infos, function(PlayerGameInfo $info) use ($id) {
+            return $info->getID() === $id;
         });
         if (count($filtered) > 0) {
-            // resetで連想配列の一番最初の値が取得できる
-            return reset($filtered);
+            return reset($filtered);// resetで連想配列の一番最初の値が取得できる
         }
 
-        // TODO
+        // SQL
+        $query = self::SELECT_QUERY;
+        $query .= "WHERE player.id = :id;";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC); // 実行結果から1行のみ取得
+        if (!$result) return null;
+
+        $info = $this->generatePlayerGameInfoFromResult($result);
+
+        // 取得結果をメモリに保存
+        $this->infos[$info->getUUID()] = $info;
+
+        return $info;
     }
 
+    /**
+     * プレイヤーのUUIDからデータを取得
+     *
+     * @param UuidInterface $uuid
+     * @return PlayerGameInfo|null
+     */
     public function findByUUID(UuidInterface $uuid): ?PlayerGameInfo
     {
+        // メモリにデータが存在している場合はそれを返す
         if (isset($this->infos[$uuid])) {
             return $this->infos[$uuid];
         }
 
+        // SQL
         $query = self::SELECT_QUERY;
         $query .= "WHERE player.uuid = :uuid;";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(":uuid", $uuid);
         $stmt->execute();
-        // 実行結果から1行のみ取得
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC); // 実行結果から1行のみ取得
+        if (!$result) return null;
 
+        $info = $this->generatePlayerGameInfoFromResult($result);
 
+        // 取得結果をメモリに保存
+        $this->infos[$uuid] = $info;
+
+        return $info;
     }
 
+    /**
+     * プレイヤーの名前からデータを取得
+     *
+     * @param string $name
+     * @return PlayerGameInfo|null
+     */
     public function findByName(string $name): ?PlayerGameInfo
     {
+        // 名前でフィルターをかける
+        $filtered = array_filter($this->infos, function(PlayerGameInfo $info) use ($name) {
+            return $info->getName() === $name;
+        });
+        if (count($filtered) > 0) {
+            return reset($filtered);// resetで連想配列の一番最初の値が取得できる
+        }
+
+        // SQL
+        $query = self::SELECT_QUERY;
+        $query .= "WHERE player.name = :name;";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(":name", $name, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC); // 実行結果から1行のみ取得
+        if (!$result) return null;
+
+        $info = $this->generatePlayerGameInfoFromResult($result);
+
+        // 取得結果をメモリに保存
+        $this->infos[$info->getUUID()] = $info;
+
+        return $info;
     }
 
+    /**
+     * Undocumented function
+     *
+     * @param PlayerGameInfo $info
+     * @return boolean
+     */
     public function store(PlayerGameInfo $info): bool
     {
 
+    }
+
+    private function generatePlayerGameInfoFromResult(array $result)
+    {
+        return new PlayerGameInfo(
+            $result["id"],
+            Uuid::fromString($result["uuid"]),
+            $result["name"],
+            $result["money"],
+            $result["level"],
+            $result["kill_count"],
+            $result["death_count"],
+            $result["play_count"],
+            $result["destroy_core_count"]
+        );
     }
 }
